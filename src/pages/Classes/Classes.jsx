267c74b9cf16afa6925c09d49a272c073abcd218
jinxsazzad <sazzad3110@gmail.com>
2023-06-09 07:17:27 +0600

    add some style on classes page

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,41 +1,49 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const Classes = () => {
-  const allClass = useLoaderData()
-  console.log(allClass)
-    const bg = false
+  const {user}= useAuth()
+  const allClass = useLoaderData();
   return (
     <div>
       <div className=" mx-auto h-full">
-      <div className="grid grid-cols-3">
-        {allClass?.map(cls=><div key={cls._id} className={`border-4 indicator my-6 ${cls.availableSeats === 0?'border-black':'border-red-400'}`}>
-          <span className="indicator-item indicator-center badge badge-secondary w-20 h-6">{cls.price}$</span>
-          <div className="card pt-4 w-full rounded-md shadow-xl  bg-white">
-            <figure>
-              <div className="avatar">
-                <div className="w-40 h-40">
-                  <img src="https://i.ibb.co/18TwhL7/Toy-superhero-avengers-png-image.png" />
-                </div>
-              </div>
-            </figure>
-            <div className="card-body flex justify-center items-center">
-              <h2 className="card-title">
-                {cls.className}
-                <span className="badge badge-secondary">New</span>
-              </h2>
-              <p>{cls.instructorName} <br />
-              Available seats: {cls.availableSeats}
-              </p>
-              <div className="card-actions">
-                <div className="badge badge-secondary btn btn-xs rounded-full">
-                  See classes
+        <div className="grid grid-cols-4 gap-2 p-2">
+          {allClass?.map((cls) => (
+            <div
+              key={cls._id}
+              className={`border-4 my-6 ${
+                cls.availableSeats === 0 ? "border-red-600" : "border-gray-400"
+              }`}
+            >
+              <div className="card pt-4 rounded-md shadow-xl  bg-white">
+                <figure>
+                  <div className="avatar">
+                    <div className="w-40 h-40">
+                      <img src={cls.classPicture} />
+                    </div>
+                  </div>
+                </figure>
+                <div className="card-body flex justify-center items-center">
+                  <h2 className="card-title">
+                    {cls.className}
+                    <span className="badge badge-secondary">New</span>
+                  </h2>
+                  <p>
+                    Instructor:{cls.instructorName} <br />
+                    Available seats: {cls.availableSeats} <br />
+                    Price:{cls.classPrice}
+                  </p>
+                  <div className="card-actions">
+                    <div className={`badge badge-secondary btn btn-xs rounded-full ${user.role === "student"? 'bg-white':' disabled'}`}>
+                      See classes
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>)}
-      </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/pages/Instructors/InstructorClasses.jsx
+++ src/pages/Instructors/InstructorClasses.jsx
@@ -5,17 +5,17 @@ const InstructorClasses = () => {
   return (
     <div className="grid grid-cols-4 p-4 gap-2 bg-primary">
       {instructorClasses?.map((cla) => (
-        <div className="card w-60 rounded-md shadow-xl text-black bg-white">
+        <div key={cla._id} className="card rounded-md shadow-xl text-black bg-white">
           <figure>
             <div className="avatar">
-              <div className="w-40 h-40">
-                <img src="https://i.ibb.co/18TwhL7/Toy-superhero-avengers-png-image.png" />
+              <div className="w-40 h-40 p-2">
+                <img src={cla.classPicture} />
               </div>
             </div>
           </figure>
           <div className="card-body flex justify-center items-center">
             <h2 className="card-title">
-              Name Alex
+              {cla.className}
               <span className="badge badge-secondary">New</span>
             </h2>
             <p>Email: alex@gamil.com</p>
